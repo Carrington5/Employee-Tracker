f9f4c364eb572d893de21c835efb550e18aa19bb
Carrington5 <88473030+Carrington5@users.noreply.github.com>
2022-01-23 21:50:31 -0600

    index.js for views and depts

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const actions = {
+    viewDept: require('./viewDept'),
+    viewRoles: require('./viewRoles'),
+    viewEmployees: require('./viewEmployees'),
+    addDept: require('./addDept'),
+    addRole: require('./addRole'),
+    addEmployee: require('./addEmployee'),
+    updateEmployee: require('./updateEmployee'),
+    removeDept: require('./removeDept')
+};
+
+module.exports = actions;
